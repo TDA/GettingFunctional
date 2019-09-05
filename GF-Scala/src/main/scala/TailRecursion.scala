79c1a592f51733209d03e2dc155d75c70e9e9927
Sai Chandramouli <schand31@asu.edu>
2019-09-04 17:31:52 -0700

    Added Scala Tail recursion code

--- GF-Scala/src/main/scala/TailRecursion.scala
+++ GF-Scala/src/main/scala/TailRecursion.scala
@@ -1,5 +1,17 @@
 package main.scala
 
-class TailRecursion {
+object TailRecursion {
 
+  @scala.annotation.tailrec
+  def fibonacciTailRecursion(number: Int, firstNumber: BigInt, secondNumber: BigInt): BigInt = {
+    if (number == 0) secondNumber else fibonacciTailRecursion(number - 1, firstNumber + secondNumber, firstNumber)
+  }
+
+  def main(args: Array[String]): Unit = {
+    val number = 100
+    val first = BigInt("0")
+    val second = BigInt("1")
+
+    println(fibonacciTailRecursion(number, firstNumber = first, secondNumber = second))
+  }
 }
