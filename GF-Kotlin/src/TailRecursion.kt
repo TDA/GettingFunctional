cc807efa6efd99abdad71a3e5d4ddf3bb166c6ec
Sai Chandramouli <schand31@asu.edu>
2019-09-04 17:19:07 -0700

    Wrote the normal and tail recursive versions of Fibonacci in Kotlin

--- GF-Kotlin/src/TailRecursion.kt
+++ GF-Kotlin/src/TailRecursion.kt
@@ -1,8 +1,27 @@
+import java.math.BigInteger
+
 fun main() {
-    val number = 5
-    println(factorial(number))
+    val number = 100
+    val first = BigInteger("0")
+    val second = BigInteger("1")
+    // This avoids the stack overflow error by having the recursive call as the last item in its
+    // function and passing in a kinda running total or accumulator. This means that at any
+    // given point, we need only one stack frame (for the most recent recursed call) and so
+    // the compiler can optimize stuff.
+    println(fibonacciTailRecursion(number, firstNumber = first, secondNumber = second))
+
+    // This would cause a StackOverflow error
+    println(fibonacciRegularRecursion(BigInteger(number.toString())))
 }
 
-fun factorial(number: Int): Int {
-    return 0
+tailrec fun fibonacciTailRecursion(number: Int, firstNumber: BigInteger, secondNumber: BigInteger): BigInteger {
+    return if (number == 0) secondNumber else fibonacciTailRecursion(number - 1, firstNumber + secondNumber, firstNumber)
+}
+
+fun fibonacciRegularRecursion(number: BigInteger): BigInteger {
+    if (number.equals(0)) {
+        return BigInteger("1");
+    } else
+        return fibonacciRegularRecursion(number.subtract(BigInteger("2"))) + fibonacciRegularRecursion(number.subtract(
+            BigInteger("1")))
 }
